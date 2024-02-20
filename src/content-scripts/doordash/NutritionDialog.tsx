1f9d720a4521339b7c60d5a4cdd51525cd4889b8
hhimanshu <dev.atwork@yahoo.com>
2024-02-21 06:07:36 +1300

    add tailwindcss

--- src/content-scripts/doordash/NutritionDialog.tsx
+++ src/content-scripts/doordash/NutritionDialog.tsx
@@ -1,12 +1,8 @@
 // NutritionDialog.tsx
-import React from 'react';
+import React from "react";
 
 const NutritionDialog: React.FC = () => {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default NutritionDialog;
